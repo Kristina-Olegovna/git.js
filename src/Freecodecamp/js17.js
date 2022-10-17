44f0e46f0e78b4cb91829b827324bf55f8b18a31
Kristina-Olegovna <kristiubchik92@gmail.com>
2022-10-17 14:34:02 +0300

    Commit

---
+++ src/Freecodecamp/js17.js
@@ -0,0 +1,17 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": ["CD", "Cassette", "LP"],
+        "gold": true
+    },
+
+    {
+        "artist": "Daft Punk",
+        "title": "Homework",
+        "release_year": 2005,
+        "formats":["CD", "Cassette", "LP"]
+
+    }
+];
